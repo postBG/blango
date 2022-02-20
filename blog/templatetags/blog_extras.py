4402b1bfd4002f37b1742a1fb64683ce75a2e300
Seung Lee <profile2697@gmail.com>
2022-02-20 10:51:47 +0000

    feat: add arguments to author_details filter

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,15 +1,20 @@
 from django import template
 from django.contrib.auth import get_user_model
+from django.utils.html import format_html
+
 user_model = get_user_model()
-
-
 register = template.Library()
 
+
 @register.filter(name="author_details")
-def author_details(author):
+def author_details(author, current_user=None):
     if not isinstance(author, user_model):
         return ""
 
-    name = f"{author.first_name} {author.last_name}" if author.first_name and author.last_name else f"{author.username}"
+    if author == current_user:
+        return format_html('<strong>me</strong>')
 
-    return f'<a href="mailto:{author.email}">{name}</a>' if author.email else name
+    has_first_and_last_name = author.first_name and author.last_name
+    name = f"{author.first_name} {author.last_name}" if has_first_and_last_name else f"{author.username}"
+
+    return format_html('<a href="mailto:{}">{}</a>', author.email, name) if author.email else name
