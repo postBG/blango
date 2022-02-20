3d09cf681c7edbf65f9ca30d3892d18a4cbfc6dd
Seung Lee <profile2697@gmail.com>
2022-02-20 14:46:05 +0000

    feat: add author_detail_tag

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -38,3 +38,27 @@ def col(extra_classes=""):
 @register.simple_tag
 def endcol():
     return format_html("</div>")
+
+
+@register.simple_tag(takes_context=True)
+def author_details_tag(context):
+    request = context["request"]
+    current_user = request.user
+    post = context["post"]
+    author = post.author
+
+    if author == current_user:
+        return format_html("<strong>me</strong>")
+
+    has_first_and_last_name = author.first_name and author.last_name
+    
+    name = f"{author.first_name} {author.last_name}" if has_first_and_last_name else f"{author.username}"
+
+    if author.email:
+        prefix = format_html('<a href="mailto:{}">', author.email)
+        suffix = format_html("</a>")
+    else:
+        prefix = ""
+        suffix = ""
+
+    return format_html("{}{}{}", prefix, name, suffix)
