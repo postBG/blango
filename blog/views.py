3f07029a1ab125345fc480e23bafe4a57bf45584
Seung Lee <profile2697@gmail.com>
2022-03-06 15:14:43 +0000

    feat: add get_ip view

--- blog/views.py
+++ blog/views.py
@@ -12,6 +12,11 @@ from blog.models import Post
 
 logger = logging.getLogger(__name__)
 
+def get_ip(request):
+  from django.http import HttpResponse
+  logger.info("IP: %s", request.META['REMOTE_ADDR'])
+  return HttpResponse(request.META['REMOTE_ADDR'])
+
 def index(request):
     posts = Post.objects.filter(published_at__lte=timezone.now())
     logger.debug("Got %d posts", len(posts))
