eaf9f040d47f83f714a6272af495c5473b3b56ec
Seung Lee <profile2697@gmail.com>
2022-03-20 16:35:24 +0000

    feat: add django Allauth

--- blango/settings.py
+++ blango/settings.py
@@ -49,12 +49,17 @@ class Dev(Configuration):
         'django.contrib.contenttypes',
         'django.contrib.sessions',
         'django.contrib.messages',
+        'django.contrib.sites',
         'django.contrib.staticfiles',
         'blog',
         'crispy_forms',
         'crispy_bootstrap5',
         'debug_toolbar',
-        'blango_auth'
+        'blango_auth',
+        'allauth',
+        'allauth.account',
+        'allauth.socialaccount',
+        'allauth.socialaccount.providers.google'
     ]
 
     MIDDLEWARE = [
@@ -187,6 +192,13 @@ class Dev(Configuration):
     # For Two-step activation
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
     ACCOUNT_ACTIVATION_DAYS = 7
+
+    # For allauth
+    SITE_ID = 1
+    ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+    ACCOUNT_EMAIL_REQUIRED = True
+    ACCOUNT_USERNAME_REQUIRED = False
+    ACCOUNT_AUTHENTICATION_METHOD = "email"
 
 
 class Prod(Dev):
