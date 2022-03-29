27aa8e08f52ec874d146629f0696e6caa07a37ea
Seung Lee <profile2697@gmail.com>
2022-03-29 15:43:06 +0000

    feat: replace custom serializers to DRF serializers

--- blango/settings.py
+++ blango/settings.py
@@ -59,7 +59,8 @@ class Dev(Configuration):
         'allauth',
         'allauth.account',
         'allauth.socialaccount',
-        'allauth.socialaccount.providers.google'
+        'allauth.socialaccount.providers.google',
+        'rest_framework'
     ]
 
     MIDDLEWARE = [
