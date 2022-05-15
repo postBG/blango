fe0e803cee9cd41a8185b47205eadc63132b72e3
Seung Lee <profile2697@gmail.com>
2022-05-15 15:49:51 +0000

    feat: inheritance, attributes, and arrow functions

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -22,3 +22,29 @@ class Greeter {
 
 const g = new Greeter('Patchy')  // Put your name here if you like
 g.greet()
+
+class DelayedGreeter extends Greeter {
+  delay = 2000
+
+  constructor (name, delay) {
+    super(name)
+    if (delay !== undefined) {
+      this.delay = delay
+    }
+  }
+
+  greet () {
+    setTimeout(
+      // Arrow function uses lexical binding
+      () => {
+        this.showGreeting(this.getGreeting())
+      }, this.delay
+    )
+  }
+}
+
+const dg2 = new DelayedGreeter('Patchy 2 Seconds')
+dg2.greet()
+
+const dg1 = new DelayedGreeter('Patchy 1 Second', 1000)
+dg1.greet()
